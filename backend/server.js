b80a124d30eb0e07809c4e1ec047fb9aa2441c05
Harinee Manikandan <harineemanikandan@gmail.com>
2025-06-03 13:49:22 +0530

    modified files

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(
         origin: ["http://localhost:5173", "https://fundora-seven.vercel.app"],
         methods: ["GET","POST","PUT","DELETE"],
         allowedHeaders:["Content-Type","Authorization"],
+        credentials: true,
     })
 );
 
